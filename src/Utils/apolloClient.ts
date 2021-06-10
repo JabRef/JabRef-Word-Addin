8464e5917d1aea46b060d367973b2cf3aebf7b24
Mohit <62339705+mohit038@users.noreply.github.com>
2021-06-11 02:02:41 +0530

    add-dotenv

--- src/Utils/apolloClient.ts
+++ src/Utils/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://jabref-online.herokuapp.com",
+  uri: process.env.API_HOST,
   cache: new InMemoryCache(),
 });
 export default client;
