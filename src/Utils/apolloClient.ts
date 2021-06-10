913b3e43f3524e5cd870b0f1ff324592364d1171
Mohit <62339705+mohit038@users.noreply.github.com>
2021-06-11 02:22:04 +0530

    update key name

--- src/Utils/apolloClient.ts
+++ src/Utils/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.API_HOST,
+  uri: process.env.API_HOST_URL,
   cache: new InMemoryCache(),
 });
 export default client;
