aed360a73a9c132b1aae860e6d6ef23bb1887219
Mohit <62339705+mohit038@users.noreply.github.com>
2021-06-20 14:52:20 +0530

    Add dotenv (#32)
    * add-dotenv
    
    * update key name
    
    * update-url
    
    * add PORT
    
    * add-change
    
    * add-babel/parser
    
    * remove-webpack-env
    
    * update-config-parameter
    
    * update-branch
    
    * fix-script
    
    * remove-run-script
    
    * fix-babel-parser
    
    * update-y.lock

--- src/Utils/apolloClient.ts
+++ src/Utils/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/api",
+  uri: process.env.API_HOST_URL || "https://jabref-online.herokuapp.com",
   cache: new InMemoryCache(),
 });
 export default client;
