e55b9585199dcd6fd8d652a758792019fbbc64fa
Mohit <62339705+mohit038@users.noreply.github.com>
2021-06-26 03:25:46 +0530

    clean-up

--- src/taskpane/pages/dashboard.tsx
+++ src/taskpane/pages/dashboard.tsx
@@ -4,29 +4,34 @@ import ReferenceList from "../components/ReferenceList";
 import SearchField from "../components/SearchField";
 // /* global Word */
 
+function containsSearchTerm(keyword: string) {
+  return function (item) {
+    return [item.title, item.author, item.year].some((str) =>
+      str ? str.toLowerCase().includes(keyword.toLowerCase().trim()) : false
+    );
+  };
+}
+function onCheckboxChange(ev: React.FormEvent<HTMLElement | HTMLInputElement>) {
+  return function (item) {
+    if (item.title === ev.currentTarget.title) {
+      return { ...item, isChecked: !item.isChecked };
+    }
+    return item;
+  };
+}
+
 function Dashboard() {
   const originalItems = data.map((item) => ({ ...item, isChecked: false }));
   const [items, setItems] = useState(originalItems);
-  const selectedItems = items.filter((items) => items.isChecked);
+  const checked = items.filter((item) => item.isChecked);
 
   const onFilterChange = (_: any, keyword: string): void => {
-    setItems(
-      originalItems.filter((item) =>
-        [item.title, item.author, item.year].some((str) =>
-          str ? str.toLowerCase().includes(keyword.toLowerCase().trim()) : false
-        )
-      )
-    );
+    setItems(originalItems.filter(containsSearchTerm(keyword)));
   };
 
   const handleToggleChange = (ev: React.FormEvent<HTMLElement | HTMLInputElement>) => {
     setItems((currentItems) => {
-      return currentItems.map((item) => {
-        if (item.title === ev.currentTarget.title) {
-          return { ...item, isChecked: !item.isChecked };
-        }
-        return item;
-      });
+      return currentItems.map(onCheckboxChange(ev));
     });
   };
 
