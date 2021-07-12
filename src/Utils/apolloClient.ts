297b8833f5ebd591aaaf9fff43527d31832dafac
Mohit <62339705+mohit038@users.noreply.github.com>
2021-07-12 12:34:58 +0530

    change-dir-name

--- src/Utils/apolloClient.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: process.env.API_HOST_URL || "https://jabref-online.herokuapp.com",
-  cache: new InMemoryCache(),
-});
-export default client;
