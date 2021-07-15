3b67d5b88542c07fe6c7db9128b96cb70374e4be
Mohit <62339705+mohit038@users.noreply.github.com>
2021-07-16 01:49:40 +0530

    turn off no-unused-vars

--- src/taskpane/components/ReferenceList.tsx
+++ src/taskpane/components/ReferenceList.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import {
   ITheme,
   getTheme,
--- src/taskpane/components/SearchField.tsx
+++ src/taskpane/components/SearchField.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { ISearchBoxStyles, SearchBox } from "@fluentui/react";
 import React, { ReactElement } from "react";
 
