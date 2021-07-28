78eb46865c04ad90fb1ea26b8aa121dd963e7555
Mohit <62339705+mohit038@users.noreply.github.com>
2021-07-29 01:40:09 +0530

    clean up

--- src/taskpane/pages/citationStyle.tsx
+++ src/taskpane/pages/citationStyle.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { FocusZone, FocusZoneDirection } from "@fluentui/react/lib/FocusZone";
 import { List } from "@fluentui/react/lib/List";
 import {
