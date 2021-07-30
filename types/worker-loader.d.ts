243bdff93acbff2d90fd8564a859093c6a42570c
Mohit <62339705+mohit038@users.noreply.github.com>
2021-07-30 22:35:05 +0530

    update branch

--- types/worker-loader.d.ts
+++ types/worker-loader.d.ts
@@ -1,6 +1,29 @@
-declare module "worker-loader!*" {
+import {
+  citationByIndexInterface,
+  referenceDataInterface,
+  Citation,
+} from "citeproc";
+
+/* eslint-disable no-unused-vars */
+declare module "*.worker.ts" {
   class WebpackWorker extends Worker {
     constructor();
+
+    onmessage: ((this: Worker, ev: WorkerEventInterface) => any) | null;
   }
+  interface WorkerEventInterface {
+    data: DataInterface;
+  }
+  interface DataInterface {
+    command: string;
+    localeName: string;
+    citationByIndex: Array<citationByIndexInterface>;
+    referenceData: Array<referenceDataInterface>;
+    styleName: string;
+    citation: Citation;
+    preCitations: Array<[string, number]>;
+    postCitations: Array<[string, number]>;
+  }
+
   export default WebpackWorker;
 }
