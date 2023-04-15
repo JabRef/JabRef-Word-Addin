ac6bcf412a940bbe25a965248cbd63c71145aa73
mohit038 <112958629+mohit038@users.noreply.github.com>
2023-04-15 19:39:56 +0530

    update config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,51 @@
-const devCerts = require("office-addin-dev-certs");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-var dotenv = require("dotenv").config({ path: __dirname + "/.env" });
+const devCerts = require('office-addin-dev-certs');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+var dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 
-const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlDev = 'https://localhost:3000/';
+const urlProd = 'https://www.contoso.com/'; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 module.exports = async (env, options) => {
-  const dev = options.mode === "development";
-  const buildType = dev ? "dev" : "prod";
+  const dev = options.mode === 'development';
+  const buildType = dev ? 'dev' : 'prod';
   const config = {
-    devtool: "source-map",
+    devtool: 'source-map',
     entry: {
-      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      vendor: ["react", "react-dom", "core-js", "@fluentui/react"],
-      taskpane: ["react-hot-loader/patch", "./src/taskpane/index.tsx"],
-      commands: "./src/commands/commands.ts",
+      polyfill: ['core-js/stable', 'regenerator-runtime/runtime'],
+      vendor: ['react', 'react-dom', 'core-js', '@fluentui/react'],
+      taskpane: ['react-hot-loader/patch', './src/taskpane/index.tsx'],
+      commands: './src/commands/commands.ts',
     },
     resolve: {
-      extensions: [".ts", ".tsx", ".html", ".js"],
+      extensions: ['.ts', '.tsx', '.html', '.js'],
+      fallback: {
+        Promise: require.resolve('es6-promise'),
+      },
     },
     module: {
       rules: [
         {
           test: /\.worker\.ts$/,
-          loader: "worker-loader",
+          loader: 'worker-loader',
         },
         {
           test: /\.tsx?$/,
-          use: ["react-hot-loader/webpack", "ts-loader"],
+          use: ['react-hot-loader/webpack', 'ts-loader'],
           exclude: /node_modules/,
         },
         {
           test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
           test: /\.(png|jpg|jpeg|gif)$/,
-          loader: "file-loader",
-          options: {
-            name: "[path][name].[ext]",
+          type: 'asset/resource',
+          generator: {
+            filename: '[path][name].[ext]',
           },
         },
       ],
@@ -50,22 +53,22 @@ module.exports = async (env, options) => {
     plugins: [
       new CleanWebpackPlugin(),
       new webpack.DefinePlugin({
-        "process.env": JSON.stringify(dotenv.parsed),
+        'process.env': JSON.stringify(dotenv.parsed),
       }),
       new CopyWebpackPlugin({
         patterns: [
           {
-            to: "taskpane.css",
-            from: "./src/taskpane/taskpane.css",
+            to: 'taskpane.css',
+            from: './src/taskpane/taskpane.css',
           },
           {
-            to: "[name]." + buildType + ".[ext]",
-            from: "manifest*.xml",
+            to: '[name].' + buildType + '.[ext]',
+            from: 'manifest*.xml',
             transform(content) {
               if (dev) {
                 return content;
               } else {
-                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
+                return content.toString().replace(new RegExp(urlDev, 'g'), urlProd);
               }
             },
           },
@@ -75,25 +78,24 @@ module.exports = async (env, options) => {
           },
         ],
       }),
-      new ExtractTextPlugin("[name].[hash].css"),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+      }),
       new HtmlWebpackPlugin({
-        filename: "taskpane.html",
-        template: "./src/taskpane/taskpane.html",
-        chunks: ["taskpane", "vendor", "polyfills"],
+        filename: 'taskpane.html',
+        template: './src/taskpane/taskpane.html',
+        chunks: ['taskpane', 'vendor', 'polyfills'],
       }),
       // Needed as Azure deployment expects an index.html file
       new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: "./src/index.html",
-        chunks: ["index"],
+        filename: 'index.html',
+        template: './src/index.html',
+        chunks: ['index'],
       }),
       new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["commands"],
-      }),
-      new webpack.ProvidePlugin({
-        Promise: ["es6-promise", "Promise"],
+        filename: 'commands.html',
+        template: './src/commands/commands.html',
+        chunks: ['commands'],
       }),
     ],
   };
@@ -102,14 +104,11 @@ module.exports = async (env, options) => {
     config.devServer = {
       hot: true,
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Origin': '*',
       },
-      https:
-        options.https !== undefined
-          ? options.https
-          : await devCerts.getHttpsServerOptions(),
+      https: options.https !== undefined ? options.https : await devCerts.getHttpsServerOptions(),
       port: process.env.npm_package_config_dev_server_port || 3000,
-    }
+    };
   }
 
   return config;
