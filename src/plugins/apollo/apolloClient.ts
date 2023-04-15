9ea304659607fffb27ae06125a864d7950524a32
Tobias Diez <code@tobiasdiez.com>
2023-04-15 15:23:20 +0200

    chore: fix url to graphql api (#569)

--- src/plugins/apollo/apolloClient.ts
+++ src/plugins/apollo/apolloClient.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env?.API_HOST_URL || 'https://jabref-online.herokuapp.com',
+  uri:
+    process.env?.API_HOST_URL ||
+    'https://mango-pebble-0224c3803-dev.westeurope.1.azurestaticapps.net',
   cache: new InMemoryCache(),
 });
 export default client;
